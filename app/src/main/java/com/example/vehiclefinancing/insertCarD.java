414076381766e3746dacc60370a8fb4676835c26
OjasMehrotra <ojas.mehrotra@student.suas.ac.in>
2023-05-19 10:47:41 +0530

    updated

---
+++ app/src/main/java/com/example/vehiclefinancing/insertCarD.java
@@ -0,0 +1,37 @@
+package com.example.vehiclefinancing;
+
+public class insertCarD {
+    String vt;
+    String m;
+    String v;
+    String s;
+    String k;
+
+    public insertCarD(String vt, String m, String v, String s, String k) {
+        this.vt = vt;
+        this.m = m;
+        this.v = v;
+        this.s = s;
+        this.k = k;
+    }
+
+    public String getVt() {
+        return vt;
+    }
+
+    public String getM() {
+        return m;
+    }
+
+    public String getV() {
+        return v;
+    }
+
+    public String getS() {
+        return s;
+    }
+
+    public String getK() {
+        return k;
+    }
+}
